6a3a34e50df5a888c29c2a51fc59cd8b812b1448
lasse <77295879+lassejlv@users.noreply.github.com>
2025-02-28 13:53:58 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,16 @@
+  // Registrer serviceworker når siden loader
+        if ('serviceWorker' in navigator) {
+            window.addEventListener('load', function() {
+                navigator.serviceWorker.register('/sw.js')
+                    .then(function(registration) {
+                        console.log('ServiceWorker registration successful:', registration.scope);
+                    })
+                    .catch(function(error) {
+                        console.log('ServiceWorker registration failed:', error);
+                    });
+            });
+        }
+
 self.addEventListener('install', function(event) {
     console.log('ServiceWorker: Installed');
 });
