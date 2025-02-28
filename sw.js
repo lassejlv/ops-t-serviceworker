660bdf795f7d94393c75fe0923c9761a88871aee
lasse <77295879+lassejlv@users.noreply.github.com>
2025-02-28 13:53:12 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', function(event) {
+    console.log('ServiceWorker: Installed');
+});
+
+self.addEventListener('activate', function(event) {
+    console.log('ServiceWorker: Activated');
+});
+
+self.addEventListener('fetch', function(event) {
+    console.log('ServiceWorker: Fetching', event.request.url);
+});
